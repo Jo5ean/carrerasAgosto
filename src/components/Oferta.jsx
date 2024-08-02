120db95c71dc48991c65ab2b7afdd7a79b072fb4
Jo5ean <josean.urbani@gmail.com>
2024-08-02 10:22:52 -0300

    Modularizacion de form & filtros y funcionalidad en carta

--- src/components/Oferta.jsx
+++ src/components/Oferta.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Card from "./Card";
 import { datita } from "../scripts/datita";
+import { facus } from "../scripts/facultades";
 
 // Función para eliminar diacríticos
 const sinDiacriticos = (function () {
@@ -11,12 +12,11 @@ const sinDiacriticos = (function () {
   return (texto) => texto.replace(re, (match) => a.charAt(de.indexOf(match)));
 })();
 
-const facultyCodes = {
-  Ingenieria: 14,
-  Comunicacion: 11,
-  Economia: 12,
-  Derecho: 13,
-};
+// Creamos un objeto para mapear codare a nombres de facultades
+const facultyNames = facus.reduce((acc, { codare, nombre }) => {
+  acc[codare] = nombre;
+  return acc;
+}, {});
 
 const Oferta = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -39,19 +39,18 @@ const Oferta = () => {
       );
 
       const matchesFacultades = filters.facultades.length
-        ? filters.facultades.includes(facultyCodes[carrera.codare])
+        ? filters.facultades.includes(carrera.codare.toString())
         : true;
       const matchesModalidad = filters.modalidad.length
-        ? filters.modalidad.includes(carrera.modalidad)
+        ? carrera.modo.some(m => filters.modalidad.includes(m.toString()))
         : true;
       const matchesTipo = filters.tipo.length
-        ? filters.tipo.includes(carrera.tipo)
+        ? filters.tipo.includes(carrera.tipcar)
         : true;
 
       return matchesSearchTerm && matchesFacultades && matchesModalidad && matchesTipo;
     });
 
-    console.log("Filtered Careers:", filtered); // Agrega este log para depurar
     setFilteredCarreras(filtered);
   }, [searchTerm, filters]);
 
@@ -71,6 +70,13 @@ const Oferta = () => {
         : prevFilters[name].filter((filterValue) => filterValue !== value);
 
       return { ...prevFilters, [name]: updatedFilterValues };
+    });
+  };
+
+  const handleRemoveFilter = (filterCategory, filterValue) => {
+    setFilters((prevFilters) => {
+      const updatedFilterValues = prevFilters[filterCategory].filter((value) => value !== filterValue);
+      return { ...prevFilters, [filterCategory]: updatedFilterValues };
     });
   };
 
@@ -106,7 +112,7 @@ const Oferta = () => {
               className="w-full pl-10 pr-4 py-2 text-gray-700 placeholder-gray-500 border border-gray-300 rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm"
             />
           </div>
-          <button type="button" onClick={toggleSidebar} className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden">
+          <button type="button" onClick={toggleSidebar} className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6">
             <span className="sr-only">Filters</span>
             <svg className="h-5 w-5" aria-hidden="true" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M2.628 1.601C5.028 1.206 7.49 1 10 1s4.973.206 7.372.601a.75.75 0 01.628.74v2.288a2.25 2.25 0 01-.659 1.59l-4.682 4.683a2.25 2.25 0 00-.659 1.59v3.037c0 .684-.31 1.33-.844 1.757l-1.937 1.55A.75.75 0 018 18.25v-5.757a2.25 2.25 0 00-.659-1.591L2.659 6.22A2.25 2.25 0 012 4.629V2.34a.75.75 0 01.628-.74z" clipRule="evenodd" />
@@ -129,37 +135,39 @@ const Oferta = () => {
             {/* Filters */}
             <form>
               <h3 className="text-sm font-medium text-gray-900">Facultades</h3>
-              <ul className="mt-2 mb-4 space-y-2">
-                {["Ingenieria", "Comunicacion", "Economia", "Derecho"].map((facultad) => (
-                  <li key={facultad}>
+              <ul className="mt-2 mb-4 space-y-1">
+                {facus.map(({ codare, nombre }) => (
+                  <li key={codare}>
                     <input
-                      id={`filter-facultad-${facultad}`}
+                      id={`filter-facultad-${nombre}`}
                       name="facultades"
-                      value={facultyCodes[facultad]}
+                      value={codare}
                       type="checkbox"
+                      checked={filters.facultades.includes(codare.toString())}
                       onChange={handleFilterChange}
                       className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                     />
-                    <label htmlFor={`filter-facultad-${facultad}`} className="ml-2 text-gray-700">
-                      {facultad}
+                    <label htmlFor={`filter-facultad-${nombre}`} className="ml-2 text-gray-700 text-xs">
+                      {nombre}
                     </label>
                   </li>
                 ))}
               </ul>
               <h3 className="text-sm font-medium text-gray-900">Modalidad</h3>
               <ul className="mt-2 mb-4 space-y-2">
-                {["Presencial", "Distancia"].map((modalidad) => (
+                {[1, 2, 7].map((modalidad) => (
                   <li key={modalidad}>
                     <input
                       id={`filter-modalidad-${modalidad}`}
                       name="modalidad"
                       value={modalidad}
                       type="checkbox"
+                      checked={filters.modalidad.includes(modalidad.toString())}
                       onChange={handleFilterChange}
                       className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                     />
                     <label htmlFor={`filter-modalidad-${modalidad}`} className="ml-2 text-gray-700">
-                      {modalidad}
+                      {modalidad === 1 ? "Presencial" : modalidad === 2 ? "Distancia" : "Mixto"}
                     </label>
                   </li>
                 ))}
@@ -173,6 +181,7 @@ const Oferta = () => {
                       name="tipo"
                       value={tipo}
                       type="checkbox"
+                      checked={filters.tipo.includes(tipo)}
                       onChange={handleFilterChange}
                       className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                     />
@@ -187,6 +196,50 @@ const Oferta = () => {
         </div>
       </form>
 
+      <div className="mt-4">
+        <div className="flex flex-wrap space-x-2 space-y-2">
+          {filters.facultades.map((facultad) => (
+            <div key={facultad} className="flex bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700">
+              {facultyNames[facultad]}
+              <button
+                className="ml-1 text-gray-500 hover:text-gray-700 focus:outline-none"
+                onClick={() => handleRemoveFilter('facultades', facultad)}
+              >
+                <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
+          ))}
+          {filters.modalidad.map((modalidad) => (
+            <div key={modalidad} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              {modalidad === "1" ? "Presencial" : modalidad === "2" ? "Distancia" : "Mixto"}
+              <button
+                className="ml-1 text-gray-500 hover:text-gray-700 focus:outline-none"
+                onClick={() => handleRemoveFilter('modalidad', modalidad)}
+              >
+                <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
+          ))}
+          {filters.tipo.map((tipo) => (
+            <div key={tipo} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              {tipo}
+              <button
+                className="ml-1 text-gray-500 hover:text-gray-700 focus:outline-none"
+                onClick={() => handleRemoveFilter('tipo', tipo)}
+              >
+                <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+
       <div className="grid grid-cols-1 sm:grid-cols-3 2xl:grid-cols-4 gap-4 mt-10">
         {filteredCarreras.map((carrera) => {
           const { codcar, nombre, codare, modo, duracion } = carrera;
@@ -200,7 +253,7 @@ const Oferta = () => {
               key={codcar?.toString()}
               codcar={codcar?.toString()}
               nombre={nombre || ""}
-              codare={codare?.toString()}
+              codare={facultyNames[codare] || ""}
               modo={modoStr}
               duracion={duracion || ""}
               nom={nombre || ""}
@@ -211,5 +264,4 @@ const Oferta = () => {
     </div>
   );
 };
-
 export default Oferta;
